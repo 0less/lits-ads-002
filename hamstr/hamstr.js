1de6a2b96ea17fed4063a6783d2e838496f69bad
Oles <oblazhivsky@gmail.com>
2015-11-10 11:09:32 +0200

    second task: hamsters

---
+++ hamstr/hamstr.js
@@ -0,0 +1,73 @@
+var fs = require("fs");
+var inputRows = fs.readFileSync('hamstr.in').toString().split(/\r?\n/);
+var food = inputRows[0];
+var hamstersCount = inputRows[1];
+var hamsters = [];
+var count = 0;
+
+for (var i=0; i<hamstersCount; i++) {
+  hamsters[i] = inputRows[i+2].split(' ');
+}
+count = checkHamsters(hamsters, hamstersCount);
+fs.writeFileSync("hamstr.out", count, 'utf8');
+
+
+function checkHamsters (hamsters, hamstersCount) {
+  var foodArray = getFoodArray(hamsters, hamstersCount);
+  var sortedFoodArray = mergeSort(foodArray);
+  var hamstersEatSum = getFoodSum(sortedFoodArray, hamstersCount);
+
+  if (hamstersEatSum > food) {
+    return checkHamsters(hamsters, --hamstersCount);
+  }
+  return hamstersCount;
+}
+
+function getFoodArray (hamsters, hamstersCount) {
+  var hamstersEat = [];
+  var hamstersLength = hamsters.length;
+
+  for (var i=0; i<hamstersLength; i++) {
+    hamstersEat[i] = +hamsters[i][0] + hamsters[i][1] * (hamstersCount-1);
+  };
+  return hamstersEat;
+}
+
+function getFoodSum (sortedFoodArray, hamstersCount) {
+  var sum = 0;
+
+  for (var i=0; i<hamstersCount; i++) {
+    sum += sortedFoodArray[i];
+  };
+  return sum;
+}
+
+function mergeSort (arr) {
+  var length = arr.length;
+  if (length < 2) return arr;
+
+  var mid = Math.floor(length/2);
+  var left = arr.slice(0, mid);
+  var right = arr.slice(mid);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge (left, right) {
+  var i = 0;
+  var j = 0;
+  var leftLength = left.length;
+  var rightLength = right.length;
+  var result = [];
+
+  while(i < leftLength && j < rightLength) {
+    if (left[i] < right[j]) {
+      result.push(left[i++]);
+    } else {
+      result.push(right[j++]);
+    }
+  }
+  result = result.concat(left.slice(i)).concat(right.slice(j));
+
+  return result;
+}

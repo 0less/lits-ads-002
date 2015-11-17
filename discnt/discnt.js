b260a4ffb7b7856aa89b93ba26aff60d377985b9
Oles <oblazhivsky@gmail.com>
2015-11-18 01:18:57 +0200

    speedup discnt

--- discnt/discnt.js
+++ discnt/discnt.js
@@ -8,33 +8,45 @@ fs.writeFileSync("discnt.out", finalPrice, 'utf8');
 
 function calcTotal (prices) {
   var finalPrice = 0;
+  var sortedPrices = reversedMergeSort(prices);
 
-  sortPrices(prices);
-
-  for (var i=0, withDiscount=Math.floor(prices.length/3); i<withDiscount; i++) {
-    prices[i] = prices[i] - (prices[i] * discount/100);
+  for (var i=0, withDiscount=Math.floor(sortedPrices.length/3); i<withDiscount; i++) {
+    sortedPrices[i] = sortedPrices[i] - (sortedPrices[i] * discount/100);
   }
 
-  for (var i=0, length=prices.length; i<length; i++) {
-    finalPrice += +prices[i];
+  for (var i=0, length=sortedPrices.length; i<length; i++) {
+    finalPrice += +sortedPrices[i];
   }
 
   return finalPrice.toFixed(2);
 }
 
-// reverted Bubble sorting
-function sortPrices (prices) {
-  for (var i=0, length=prices.length; i<length; i++) {
-    for (var j=1; j<(length-i); j++) {
-      if (+prices[j] > +prices[j-1]) {
-        swap(prices, j, j-1);
-      }
-    }
-  }
+function reversedMergeSort (arr) {
+  var length = arr.length;
+  if (length < 2) return arr;
+
+  var mid = Math.floor(length/2);
+  var left = arr.slice(0, mid);
+  var right = arr.slice(mid);
+
+  return merge(reversedMergeSort(left), reversedMergeSort(right));
 }
 
-function swap (arr, first, second) {
-  var tmp = arr[first];
-  arr[first] = arr[second];
-  arr[second] = tmp;
+function merge (left, right) {
+  var i = 0;
+  var j = 0;
+  var leftLength = left.length;
+  var rightLength = right.length;
+  var result = [];
+
+  while(i < leftLength && j < rightLength) {
+    if (+left[i] > +right[j]) {
+      result.push(left[i++]);
+    } else {
+      result.push(right[j++]);
+    }
+  }
+  result = result.concat(left.slice(i)).concat(right.slice(j));
+
+  return result;
 }

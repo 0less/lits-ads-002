3e372b18151e176cda0c93164d4dc1e9b85466ab
Oles <oblazhivsky@gmail.com>
2016-01-20 03:13:01 +0200

    small updates

--- career/career.js
+++ career/career.js
@@ -4,35 +4,27 @@
   var rowsCount = inputRows[0];
 
   var rows = [];
-  var solutions = [];
   var visited = [];
+  var bestResult = 0;
   for (var i=0; i<rowsCount; i++) {
     rows[i] = inputRows[i+1].split(' ');
   }
 
-  addSolutions(0, 0, Number(rows[0][0]));
-  fs.writeFileSync("career.out", getMaxValue(), 'utf8');
+  findBestResult(0, 0, Number(rows[0][0]));
+  fs.writeFileSync("career.out", bestResult, 'utf8');
 
-  function getMaxValue() {
-    var max = 0;
-    for (var i = 0; i < solutions.length; i++ ) {
-      if (solutions[i] > max) max = solutions[i];
-    }
-    return max;
-  }
-
-  function addSolutions(row, col, prevSolution) {
+  function findBestResult(row, col, prevSolution) {
     if (visited[row] && visited[row][col] && visited[row][col] >= prevSolution) return;
 
     if (!visited[row]) visited[row] = [];
     visited[row][col] = prevSolution;
 
     if (!rows[row+1]) {
-      solutions.push(prevSolution);
+      if (prevSolution > bestResult) bestResult = prevSolution;
       return;
     }
 
-    addSolutions(row+1, col, prevSolution + Number(rows[row+1][col]));
-    addSolutions(row+1, col+1, prevSolution + Number(rows[row+1][col+1]));
+    findBestResult(row+1, col,   prevSolution + Number(rows[row+1][col]));
+    findBestResult(row+1, col+1, prevSolution + Number(rows[row+1][col+1]));
   }
 })();
